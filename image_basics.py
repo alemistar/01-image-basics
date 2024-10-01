11593c8ee85934927d33ae852762b4128bd16740
alemistar <82665798+alemistar@users.noreply.github.com>
2024-10-01 17:39:06 +0200

    Update extract_feature_median to try solve the hmk red cross

--- image_basics.py
+++ image_basics.py
@@ -148,7 +148,7 @@ def extract_feature_median(img):
     EXTRACT_FEATURE_MEDIAN:
     # todo: apply median filter to image (hint: 'Median')
     """
-    median_img = sitk.Median(img)
+    median_img = sitk.Median(img, [1, 1, 1])
 
     return median_img
 
